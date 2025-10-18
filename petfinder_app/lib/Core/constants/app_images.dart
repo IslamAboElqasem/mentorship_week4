864ea2ad5da43b81df5b778f13c5d6de4b9e1212
Islam Hesham <heshamislam556@gmail.com>
2025-10-18 21:13:38 +0300

    Add SVG assets and constants for icons, images, and fonts; implement routing structure
    - Added new SVG files for messages, notifications, profile circle, search, and settings.
    - Created constants for SVG paths in `svgs.dart`.
    - Introduced `AppImages` class for image asset paths.
    - Added `FontFamily` class for font constants.
    - Implemented `AppRouter` for routing management.
    - Updated `main.dart` to integrate routing and screen util.
    - Updated `pubspec.yaml` to include `flutter_screenutil` and asset paths.

---
+++ petfinder_app/lib/Core/constants/app_images.dart
@@ -0,0 +1,4 @@
+class AppImages {
+  static const String onboardingImage =
+      'assets/images/76acd944660dd48d6bb3785fe2100da1fe53de62.png';
+}
---
+++ petfinder_app/lib/Core/constants/svgs.dart
@@ -0,0 +1,11 @@
+class Svgs {
+  static const String logoIcon = 'assets/svgs/logo.svg';
+  static const String homeIcon = 'assets/svgs/home-2.svg';
+  static const String petsIcon = 'assets/svgs/ic_outline-pets.svg';
+  static const String locationIcon = 'assets/svgs/location.svg';
+  static const String messagesIcon = 'assets/svgs/messages.svg';
+  static const String notificationIcon = 'assets/svgs/notification.svg';
+  static const String profileIcon = 'assets/svgs/profile-circle.svg';
+  static const String searchIcon = 'assets/svgs/back-arrow.svg';
+  static const String filterIcon = 'assets/svgs/setting-5.svg';
+}
