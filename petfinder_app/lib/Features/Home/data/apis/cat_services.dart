130e37c0a5a3eab80e62b601ee46dee4f38c9ef9
Islam Hesham <heshamislam556@gmail.com>
2025-10-21 01:52:31 +0300

    Add error handling to CatService for failed cat data retrieval

--- petfinder_app/lib/Features/Home/data/apis/cat_services.dart
+++ petfinder_app/lib/Features/Home/data/apis/cat_services.dart
@@ -10,6 +10,10 @@ class CatService {
         'https://api.thecatapi.com/v1/images/search?limit=10&&breed_ids=beng&api_key=live_NhHU168ZwOcz8VMdLwQF1XsXSeEtfSjBL7aI1bvBn9X31qgLsw6Ym6BPTv0v3toY';
 
     final response = await dio.get(url);
-    return response.data;
+    if (response.statusCode == 200) {
+      return response.data;
+    } else {
+      throw Exception('Failed to load cats');
+    }
   }
 }
