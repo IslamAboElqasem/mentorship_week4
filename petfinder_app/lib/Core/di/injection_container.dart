eb9ab20264c242527691102a588ce64a02e84706
Islam Hesham <heshamislam556@gmail.com>
2025-10-21 03:23:11 +0300

    Replace Dio with http.Client in CatService and update service locator

--- petfinder_app/lib/Core/di/injection_container.dart
+++ petfinder_app/lib/Core/di/injection_container.dart
@@ -1,12 +1,12 @@
-import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
 import 'package:petfinder_app/Features/Home/data/apis/cat_services.dart';
 import 'package:petfinder_app/Features/Home/data/repo/cat_repository.dart';
 
 final sl = GetIt.instance;
 
 Future<void> setupServiceLocator() async {
-  sl.registerLazySingleton(() => Dio());
+  sl.registerLazySingleton<http.Client>(() => http.Client());
   sl.registerLazySingleton(() => CatService(sl()));
   sl.registerLazySingleton(() => CatRepository(sl()));
 }
--- petfinder_app/lib/Features/Home/data/apis/cat_services.dart
+++ petfinder_app/lib/Features/Home/data/apis/cat_services.dart
@@ -1,17 +1,19 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
 
 class CatService {
-  final Dio dio;
+  final http.Client client;
 
-  CatService(this.dio);
+  CatService(this.client);
 
   Future<List<dynamic>> getCatsResponse() async {
     const url =
         'https://api.thecatapi.com/v1/images/search?limit=10&&breed_ids=beng&api_key=live_NhHU168ZwOcz8VMdLwQF1XsXSeEtfSjBL7aI1bvBn9X31qgLsw6Ym6BPTv0v3toY';
 
-    final response = await dio.get(url);
+    final response = await client.get(Uri.parse(url));
     if (response.statusCode == 200) {
-      return response.data;
+      return jsonDecode(response.body) as List<dynamic>;
     } else {
       throw Exception('Failed to load cats');
     }
